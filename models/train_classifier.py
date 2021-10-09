d5b344b7e805742bf5e196fbc7d0bd46d4b5dfac
trhgvinh <62598902+trhgvinh@users.noreply.github.com>
2021-10-09 12:49:30 +0700

    Add files via upload

--- models/train_classifier.py
+++ models/train_classifier.py
@@ -1,24 +1,119 @@
 import sys
+import sqlite3
 
+import re
+import numpy as np
+import pandas as pd
+
+from nltk.tokenize import word_tokenize
+from nltk.stem import WordNetLemmatizer
+
+from sklearn.metrics import confusion_matrix
+from sklearn.model_selection import GridSearchCV
+from sklearn.ensemble import RandomForestClassifier
+
+from sklearn.datasets import make_multilabel_classification
+
+from sklearn.multioutput import MultiOutputClassifier
+from sklearn.neighbors import KNeighborsClassifier
+
+from sklearn.model_selection import train_test_split
+from sklearn.pipeline import Pipeline, FeatureUnion
+from sklearn.base import BaseEstimator, TransformerMixin
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+
+import joblib
 
 def load_data(database_filepath):
-    pass
+    conn = sqlite3.connect(database_filepath)
+    df = pd.read_sql('SELECT * FROM message', conn)
+    
+    categories = df.categories.str.split(";", expand=True)
+    row = categories.iloc[0,:]
+    category_names = row.apply(lambda x: x[:-2]).tolist()
+    
+    categories.columns = category_names
+    for column in categories:
+        # set each value to be the last character of the string
+        categories[column] = pd.Series(categories[column], dtype=str)
+        categories[column]=categories[column].str[-1:]
+    
+        # convert column from string to numeric
+        categories[column] = pd.to_numeric(categories[column])
+    
+    df = pd.concat([df.drop('categories', axis = 1), categories], axis = 1)
+    df.related.replace(2,1,inplace=True)
+    
+    X = df.message.values
+    Y = categories.values
+    
+    return X, Y, category_names
 
 
 def tokenize(text):
-    pass
+    url_regex = 'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+'
+    detected_urls = re.findall(url_regex, text)
+    for url in detected_urls:
+        text = text.replace(url, "urlplaceholder")
 
+    tokens = word_tokenize(text)
+    lemmatizer = WordNetLemmatizer()
 
-def build_model():
-    pass
+    clean_tokens = []
+    for tok in tokens:
+        clean_tok = lemmatizer.lemmatize(tok).lower().strip()
+        clean_tokens.append(clean_tok)
+
+    return clean_tokens
+
+class StartingVerbExtractor(BaseEstimator, TransformerMixin):
+
+    def starting_verb(self, text):
+        sentence_list = nltk.sent_tokenize(text)
+        for sentence in sentence_list:
+            pos_tags = nltk.pos_tag(tokenize(sentence))
+            first_word, first_tag = pos_tags[0]
+            if first_tag in ['VB', 'VBP'] or first_word == 'RT':
+                return True
+        return False
+
+    def fit(self, x, y=None):
+        return self
+
+    def transform(self, X):
+        X_tagged = pd.Series(X).apply(self.starting_verb)
+        return pd.DataFrame(X_tagged)
+
+def build_model():    
+    pipeline = Pipeline([
+                ('vect', CountVectorizer(tokenizer=tokenize)),
+                ('tfidf', TfidfTransformer()),
+                ('clf', MultiOutputClassifier(RandomForestClassifier()) )
+    ])
+    
+    # specify parameters for grid search
+    parameters = {
+        'vect__max_df': ([0.5]),
+        'tfidf__use_idf': ([False]),
+    }
+
+    # create grid search object
+    cv =  GridSearchCV(pipeline, param_grid=parameters,verbose=2)
+    return cv
 
 
 def evaluate_model(model, X_test, Y_test, category_names):
-    pass
+    Y_pred = model.predict(X_test)
+    labels = np.unique(Y_pred)
+    accuracy = (Y_pred == Y_test).mean()
+
+    print("Labels:", labels)
+    print("Accuracy:", accuracy)
+    print("\nBest Parameters:", model.best_params_)
 
 
 def save_model(model, model_filepath):
-    pass
+    joblib.dump(model, model_filepath)
 
 
 def main():
